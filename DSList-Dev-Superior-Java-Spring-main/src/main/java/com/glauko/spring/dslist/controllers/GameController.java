5b16176fffe1e8af357379ad6c4d5db736d555ee
joseglauko <glauko10dev@gmail.com>
2025-09-09 22:43:56 -0300

    Deploy ready!

---
+++ DSList-Dev-Superior-Java-Spring-main/src/main/java/com/glauko/spring/dslist/controllers/GameController.java
@@ -0,0 +1,30 @@
+package com.glauko.spring.dslist.controllers;
+
+import com.glauko.spring.dslist.dtos.GameDTO;
+import com.glauko.spring.dslist.dtos.GameMinDTO;
+import com.glauko.spring.dslist.entities.Game;
+import com.glauko.spring.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+@CrossOrigin(origins = "http://localhost:4200") // Permitir requisições do Angular
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable Long id) {return gameService.findById(id);}
+
+
+
+}
